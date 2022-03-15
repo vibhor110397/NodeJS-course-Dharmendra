8dc4d32907f22c7c6062228e10737ea01a328d41
Dharmendra Dubey <dubey.dharmendra@tftus.com>
2022-03-15 11:42:57 +0530

    SERVER CREATION

---
+++ createServer.js
@@ -0,0 +1,15 @@
+
+// **********Create Server*********
+
+
+const http=require('http');
+
+
+const server=http.createServer((req,res) => {
+    // console.log(req)
+    res.end("hello from the server!")
+});
+
+server.listen(8000,'127.0.0.1',() => {
+    console.log('listening to requests on port 8000');
+});
